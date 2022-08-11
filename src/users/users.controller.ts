bb041b151de602cdec3075752a08951aecdb8967
viveloper <ucsik1234@gmail.com>
2022-08-11 21:17:20 +0900

    user service interface

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,44 +1,32 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Query,
-} from '@nestjs/common';
-import { UsersService } from './users.service';
+import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-import { GetUsersDto } from './dto/get-users.dto';
+import { VerifyEmailDto } from './dto/verify-email.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserInfo } from './dto/user-info.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
-
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async create(@Body() dto: CreateUserDto) {
+    console.log(dto);
+    return;
   }
 
-  @Get()
-  findAll(@Query() getUsersDto: GetUsersDto) {
-    return this.usersService.findAll(getUsersDto);
+  @Post('/email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto) {
+    console.log(dto);
+    return;
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto) {
+    console.log(dto);
+    return;
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @Get('/:id')
+  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
+    console.log(userId);
+    return;
   }
 }
