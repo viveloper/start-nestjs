55de204c2687995d5cd84cda4da01e2f25288f7b
viveloper <ucsik1234@gmail.com>
2022-08-11 12:13:24 +0900

    feat: add users resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
+  imports: [UsersModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
